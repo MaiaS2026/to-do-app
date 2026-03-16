b709b187410da0fa6f5d1151ce5299869fec5e40
MaiaS2026 <maia.salgado2026@gbwl.org>
2026-03-16 16:39:33 +0000

    styled it nicely

--- app.py
+++ app.py
@@ -37,13 +37,14 @@ def index():
     return render_template('index.html', tasks=all_tasks)
 
 
-
-
-
-
-
-
-
+# Route for deleting tasks from db
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect('/')
 
 
 
